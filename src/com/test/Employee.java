0c25d72ad1ff25305b07889c255768e966c9f97c
DivyaDp04 <prathibadsp@gmail.com>
2022-09-03 23:30:08 +0530

    New message

---
+++ src/com/test/Employee.java
@@ -0,0 +1,20 @@
+package com.test;
+
+public class Employee {
+	
+	public void empId() {
+
+		System.out.println("Employee id is 37324");
+	}
+	public void empName() {
+
+		System.out.println("Employee name is Renu");
+	}
+	public static void main(String[] args) {
+		
+		Employee a= new Employee();
+		a.empId();
+		a.empName();
+	}
+
+}
