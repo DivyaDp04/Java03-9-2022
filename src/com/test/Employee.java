b8e1d3ff0964b5da2b00e331889394f545a3b590
DivyaDp04 <prathibadsp@gmail.com>
2022-09-05 15:48:06 +0530

    B had worked

--- src/com/test/Employee.java
+++ src/com/test/Employee.java
@@ -1,41 +1,46 @@
 package com.test;
 
 public class Employee {
-	
+
 	public void empId() {
 
 		System.out.println("Employee id is 37324");
 	}
+
 	public void empName() {
 
 		System.out.println("Employee name is Renu");
 	}
+
 	public void empCode() {
 
 		System.out.println("Employee code is 762785381");
 	}
-	public void empCodes() {
+
+	public void empCode1() {
 
 		System.out.println("Employee code is 762785381");
 	}
-	public void add() {
-		System.out.println("Employee code is add");
-	}
-	public void sample() {
 
-		System.out.println("Employee Sample");
-	}
-	public void sub() {
+	public void empCode2() {
 
-		System.out.println("Employee String");
+		System.out.println("Employee code is 762785381");
 	}
+
+	public void empCode3() {
+
+		System.out.println("Employee code is 762785381");
+	}
+
 	public static void main(String[] args) {
-		
-		Employee a= new Employee();
+
+		Employee a = new Employee();
 		a.empId();
 		a.empName();
 		a.empCode();
-		a.empCodes();
+		a.empCode1();
+		a.empCode2();
+		a.empCode3();
 	}
 
 }
